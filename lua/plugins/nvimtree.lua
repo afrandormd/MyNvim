77e2b16fa5646c3d66b59cc1f6078278627f7d90
afrandormd <afrandormd@gmail.com>
2026-04-23 07:58:59 +0700

    chore: show hidden files in nvimtree

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,22 +1,34 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {
-        -- nvimtree position
-        view = {
-            side = "right", -- move to ("left" or "right")
-            -- width = 30, -- optional
-        },
-    }
-    -- toggle nvimtree
-    vim.keymap.set("n", "<leader>e", "<cmd> NvimTreeToggle <CR>", {})
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({
+			-- nvimtree position
+			view = {
+				side = "right", -- move to ("left" or "right")
+				-- width = 30, -- optional
+			},
+			filters = {
+				enable = true,
+				dotfiles = false,
+				git_ignored = true,
+			},
+			renderer = {
+				icons = {
+					show = {
+						hidden = true,
+					},
+				},
+			},
+		})
+		-- toggle nvimtree
+		vim.keymap.set("n", "<leader>e", "<cmd> NvimTreeToggle <CR>", {})
 
-    -- nvimtree focus
-    vim.keymap.set("n", "<leader>o", "<cmd> NvimTreeFocus <CR>", {})
-  end,
+		-- nvimtree focus
+		vim.keymap.set("n", "<leader>o", "<cmd> NvimTreeFocus <CR>", {})
+	end,
 }
